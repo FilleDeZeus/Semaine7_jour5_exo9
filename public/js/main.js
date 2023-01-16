2caf2663b444c6398195e2fe730453f1aee51d71
FilleDeZeus <enide26@hotmail.fr>
2023-01-16 12:22:15 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -10,19 +10,19 @@ let elements = document.querySelectorAll(`#liste-soft-skills > h2`)
 console.log(elements);
 
 //EXERCICE 2 
-for (const key in softSkills) {
+for (const property in softSkills) {
     elements.forEach(element => {
-        if(key == element.innerText){
-            element.textContent += ` ${softSkills[key]}`;
+        if(property == element.innerText){
+            element.textContent += ` ${softSkills[property]}`;
 
-            if (Number(softSkills[key].slice(0, -1))<50) {
+            if (Number(softSkills[property].slice(0, -1))<50) {
                 element.style.backgroundColor = "red";
 
-            } else if (Number(softSkills[key].slice(0, -1))>50  && Number(softSkills[key].slice(0, -1))!=100){
+            } else if (Number(softSkills[property].slice(0, -1))>50  && Number(softSkills[property].slice(0, -1))!=100){
                 element.style.backgroundColor = "green";
                 element.style.color ="white";
 
-            } else if (Number(softSkills[key].slice(0, -1))==100){
+            } else if (Number(softSkills[property].slice(0, -1))==100){
                 element.style.backgroundColor = "gold";
             }
         }
